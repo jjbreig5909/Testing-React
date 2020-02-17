888c49cd1953459c02d3d9022874ebf0598e56a6
Jeff Breig <jjbreig@gmail.com>
2020-02-16 21:23:34 -0500

    added basic tests

--- star-wars-app/src/App.test.js
+++ star-wars-app/src/App.test.js
@@ -6,31 +6,7 @@ import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
-// Mock the "axios" library for any code that imports it.
-// So any imports now return this instead of the actual axios call.
-// Helps our tests to be consistent, and integration tests rather than E2E.
-// jest.mock("axios", () => {
-//     return {
-//         get: jest.fn(() => Promise.resolve({
-//                 characters: ["foo.jpg", "bar.jpg"]
-//             }
-//         ))
-//     }
-// })
-
-// this just allows react-testing-library to do some
-// routine cleanup after each test runs (to reset the DOM and such)
 afterEach(rtl.cleanup);
-
-// test("Made API call", async () => {
-//     const wrapper = rtl.render(<App />);
-//     // This waits for our initial useEffect async operation to run,
-//     // which is what makes our API call
-//     await wrapper.findAllByAltText(/dog/i);
-
-//     // Since our mocked axios.get call is a spy, we can see what it has been up to
-//     expect(axios.get).toHaveBeenCalled()
-// })
 
 test('Do buttons render?', () => {
     const { getByTestId } = rtl.render(<App />);
@@ -43,3 +19,5 @@ test('Is the logo there?', () => {
     const element = wrapper.getByAltText(/logo/i);
     expect(element).toBeVisible();
 })
+
+
